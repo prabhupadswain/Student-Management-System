1f5e217b6d5451f65568ff93ebc3482c17aa8469
prabhupadswain <prabhupad.swain@outlook.com>
2022-01-27 22:04:06 +0530

    App.js component modified to import Navigation and other components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 //import logo from './logo.svg';
 
 import './App.css';
+import Navbar from './components/Navbar';
+import HomePage from './components/HomePage';
+import Students from './components/Students';
+import AddStudent from './components/AddStudent';
 
 function App() {
   //<img src={logo} className="App-logo" alt="logo" />
   return (
-    <div className="App">
-      <header className="App-header">
-        Hello , World
-      </header>
+    <div>
+     
+        <Navbar />
+        <HomePage />
+        <Students />
+        <AddStudent />
+     
     </div>
   );
 }
